9406727826873b8cb3c3230e9a768b33750011ec
redark6 <redark66@gmail.com>
2022-04-27 20:30:49 +0200

    firts commit

---
+++ src/main/java/fr/esgi/cookRecipe/CookRecipeApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.cookRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/esgi/cookRecipe/CookRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.cookRecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
